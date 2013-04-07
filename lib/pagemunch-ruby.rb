7be801b80001075bbd1c1e0f1c8956ae92c4f72a
Luke <email@luke-roberts.info>
2013-04-07 11:39:18 +0100

    empty gem cerated

---
+++ lib/pagemunch-ruby.rb
@@ -0,0 +1,7 @@
+require "pagemunch-ruby/version"
+
+module Pagemunch
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ lib/pagemunch-ruby/version.rb
@@ -0,0 +1,5 @@
+module Pagemunch
+  module Ruby
+    VERSION = "0.0.1"
+  end
+end
